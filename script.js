af2137b328c0a6a85fbdfe22f6736210276f6802
roberttmello <roberttmello@protonmail.com>
2022-04-21 13:14:54 -0300

    removeObstacle function implemented

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const dino = document.querySelector('.dino');
+  const background = document.querySelector('.background');
+  const alert = document.getElementById('alert');
   let isJumping = false;
+  let isGameOver = false;
   let gravity = 0.9;
 
   function control(event) {
@@ -14,13 +17,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  let position = 0;
+  // If any key is pressed control function is called
+  document.addEventListener('keydown', control);
+
+  let dinoPosition = 0;
 
   function jump() {
     let count = 0;
 
     let upTimerId = setInterval(function () {
-      
+
       if (count === 15) {
         // Stop going up
         clearInterval(upTimerId);
@@ -32,19 +38,19 @@ document.addEventListener('DOMContentLoaded', () => {
             isJumping = false;
           }
           // Move down
-          position -= 5;
+          dinoPosition -= 5;
           count--;
-          position = position * gravity;
-          dino.style.bottom = position + 'px';
+          dinoPosition = dinoPosition * gravity;
+          dino.style.bottom = dinoPosition + 'px';
           console.log(dino.style.bottom);
         }, 15);
       }
 
       // Move up
-      position += 30;
+      dinoPosition += 30;
       count++;
-      position = position * gravity;
-      dino.style.bottom = position + 'px';
+      dinoPosition = dinoPosition * gravity;
+      dino.style.bottom = dinoPosition + 'px';
       console.log(dino.style.bottom);
     }, 15);
 
@@ -52,13 +58,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function generateObstacles() {
 
+    let randomTime = Math.random() * 4000;
+
+    let obstaclePosition = 1000;
+    const obstacle = document.createElement('div');
+    if(!isGameOver) obstacle.classList.add('obstacle');
+    background.appendChild(obstacle);
+    obstacle.style.left = obstaclePosition + 'px';
+
+    let timerId = setInterval(function () {
+      if (obstaclePosition > 0 && obstaclePosition < 60 && dinoPosition < 60) {
+        clearInterval(timerId);
+        alert.innerHTML = 'Game Over';
+        isGameOver = true;
+        // Remove all children
+        while (background.firstChild) {
+          background.removeChild(background.lastChild);
+        }
+      }
+
+      obstaclePosition -= 10;
+      obstacle.style.left = obstaclePosition + 'px';
+
+    }, 20);
+
+    if(!isGameOver) setTimeout(generateObstacles, randomTime);
+
   }
 
+  generateObstacles();
 
-
-
-
-  // If any key is pressed control function is called
-  document.addEventListener('keydown', control);
 
 });
